c758f68e1b6b04e87607b93fe25f0eb645ef1eb6
Adam Hammouche <ahammouche@hotmail.com>
2023-07-30 19:03:58 +0200

    creation du portfolio competences et profil

--- src/App.jsx
+++ src/App.jsx
@@ -2,47 +2,19 @@ import React, { useRef, useEffect, useState } from 'react';
 import Navbar from './components/Navbar';
 import Cover from './components/Cover';
 import Works from './components/Works';
+import Portfolio from './components/Portfolio';
+import Profil from './components/Profil';
 
 function App() {
-  const homeRef = useRef(null);
-  const worksRef = useRef(null);
-  // Ajoutez d'autres références pour les autres sections ici
-
-  const [activeSection, setActiveSection] = useState('Accueil');
-
-  useEffect(() => {
-    const handleScroll = () => {
-      const scrolly = window.scrollY;
-      const homeTop = homeRef.current.offsetTop;
-      const worksTop = worksRef.current.offsetTop;
-      // Calculez la position de chaque section ici
-
-      // Déterminez quelle section est actuellement visible en fonction de la position du défilement
-      if (scrolly >= homeTop && scrolly < worksTop) {
-        setActiveSection('Accueil');
-      } else if (scrolly >= worksTop) {
-        setActiveSection('MonTravail');
-      }
-      // Ajoutez des conditions pour les autres sections ici
-    };
-
-    window.addEventListener('scroll', handleScroll);
-
-    // Nettoyage : supprimer l'écouteur d'événements lors du démontage du composant
-    return () => {
-      window.removeEventListener('scroll', handleScroll);
-    };
-  }, []);
 
   return (
     <>
-      <Navbar activeItem={activeSection} />
-      <div ref={homeRef}>
-        <Cover />
-      </div>
-      <div ref={worksRef}>
-        <Works />
-      </div>
+      <Navbar/>
+      <Cover />
+      <Works />
+      <Portfolio/>
+      <Profil/>
+      
     </>
   );
 }
