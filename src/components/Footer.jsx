69571071215a7761a3f33f3097846070bf8dcd7a
Adam Hammouche <ahammouche@hotmail.com>
2023-08-04 20:33:22 +0200

    feat-creation du composant contact

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,15 +1,22 @@
 import React from 'react'
+import "../components/footer.css"
 
 export default function Footer() {
   return (
-    <section id='Contact'>
-        <div>
-            <div>
-                <div></div>
-                <div></div>
-                <p></p>
+    <section id='Contact' className='contact'>
+      <div className='contact-container'>
+        <div className='contact-details'>
+            <div className='contact-title'>
+              <h2>Me contacter ici</h2>
             </div>
+            <div className='contact-email'>
+              <a href='mailto:ahammouche@hotmail.com'>
+                <img className='email-icon' src='/public/email.png'></img>
+              </a>
+            </div>
+            <p>© 2023 Adam HAMMOUCHE, Tous droits réservés.</p>
         </div>
+      </div>
     </section>
   )
 }
